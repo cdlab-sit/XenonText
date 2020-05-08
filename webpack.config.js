fa5fd0d049f7032dbe14a9893759f713e587c090
higurashi-takuto <contact@hgrs.me>
2020-05-09 08:10:18 +0900

    React の追加

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry:{
+      main: './src/main/index.js',
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    target:'electron-main',
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader'
+      }]
+    }
+  },
+  {
+    entry: './src/renderer/app.jsx',
+    output: {
+      filename: 'renderer.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    target:'electron-renderer',
+    module: {
+      rules: [{
+        test: /\.jsx$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader'
+      }]
+    }
+  }
+];
