7f387e9e20515d6126967c025b9fd6de8c8158b2
sota <sota@gvpn164179.sras.sic.shibaura-it.ac.jp>
2020-05-11 22:46:46 +0900

    TitleBarの読み込み追加

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,7 +1,24 @@
 import React from "react"
 import { render } from "react-dom"
+import TitleBar from "./components/TitleBar"
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <TitleBar />
+      </div>
+
+    );
+  }
+}
 
 render(
-  <h1>Hello, React!</h1>,
+  <App />,
   document.getElementById('app')
 )
+
