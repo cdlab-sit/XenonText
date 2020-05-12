9556bdad5493a6f7d088e5c7e289a2df176dad4c
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-05-12 19:39:24 +0900

    コンポーネントの追加

---
+++ src/renderer/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer () {
+
+    const footer = "Footer";
+
+    return (
+        <h2>
+            {footer}
+        </h2>
+    );
+
+}
---
+++ src/renderer/components/sideBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SideBar () {
+
+    const sidebar = "SideBar";
+
+    return (
+        <h2>
+            {sidebar}
+        </h2>
+    );
+
+}
