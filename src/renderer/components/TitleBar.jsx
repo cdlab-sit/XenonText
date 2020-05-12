245e5806842befedaf53b93dc0510d4d57532719
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-05-12 21:50:53 +0900

    色, 文字位置, などの見た目を本仕様に変更

--- src/renderer/components/TitleBar.jsx
+++ src/renderer/components/TitleBar.jsx
@@ -5,9 +5,17 @@ export default function TitleBar () {
     const title = "TitleBar";
 
     return (
-        <h2>
-            {title}
-        </h2>
+        <header className="w-full h-6 absoluto top-0">
+            <h1
+                className="
+                    text-xs text-center text-white
+                    leading-6 font-medium
+                "
+            >
+                {title}
+            </h1>
+        </header>
+
     );
 
 }
