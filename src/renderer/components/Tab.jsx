21caab85ccb7a14b92bd4f3dd6061df748944abf
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-05-15 15:49:44 +0900

    Tab.jsxの追加

---
+++ src/renderer/components/Tab.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Tabs () {
+
+    const item = "Untitled";
+
+    return (
+        <div className="bg-gray-900 h-8 w-40">
+            <h1 className="
+                text-xs text-gray-300
+                my-1 ml-2 leading-6
+                w-32 h-6
+            ">
+                {item}
+            </h1>
+        </div>
+    );
+
+}
+
