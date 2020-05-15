914be1bbd61c6385aad8b32e4b001e76d77c2405
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-05-15 16:48:52 +0900

    h1 -> h2 変更

--- src/renderer/components/Tab.jsx
+++ src/renderer/components/Tab.jsx
@@ -6,14 +6,14 @@ export default function Tabs () {
 
     return (
         <div className="bg-gray-900 h-8 w-40">
-            <h1 className="
+            <h2 className="
                 text-xs text-gray-300
                 my-1 ml-2 leading-6
                 w-32 h-6
                 "
             >
                 {item}
-            </h1>
+            </h2>
         </div>
     );
 
