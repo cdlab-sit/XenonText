dae25bd0f8d7f15a0770cf079c123f1b50fdae55
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-07-06 14:58:22 +0900

    #34 の修正

--- src/renderer/components/EditArea.jsx
+++ src/renderer/components/EditArea.jsx
@@ -1,38 +1,58 @@
-/* eslint-disable react/jsx-no-bind */
 import "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "./theme-xenon";
+import React, {useCallback} from "react";
 import AceEditor from "react-ace";
-import React from "react";
 import {setCharCount} from "../reducks/charCount/actions";
 import {useDispatch} from "react-redux";
+
+const myRef = React.createRef();
+
+// eslint-disable-next-line one-var
+const getAllLength = () => {
+
+        const text = myRef.current.editor.getValue();
+        return text.length;
+
+    },
+    getSelectedLength = (select) => {
+
+        const empty = select.isEmpty();
+        let SelectedLength = 0;
+        if (!empty) {
+
+            const text = myRef.current.editor.getSelectedText();
+            SelectedLength = text.length;
+
+        } else if (empty) {
+
+            SelectedLength = getAllLength();
+
+        }
+        return SelectedLength;
+
+    };
 
 export default function EditArea () {
 
     const dispatch = useDispatch(),
-        myRef = React.createRef(),
-        onChange = () => {
+        updateChar = (length) => {
 
-            const text = myRef.current.editor.getValue();
-            dispatch(setCharCount(text.length));
-
-        },
-        onSelectionChange = (select) => {
-
-            const empty = select.isEmpty();
-            if (!empty) {
-
-                const selectedText = myRef.current.editor.getSelectedText();
-                dispatch(setCharCount(selectedText.length));
-
-            } else if (empty) {
-
-                onChange();
-
-            }
+            dispatch(setCharCount(length));
 
         };
 
+    // eslint-disable-next-line one-var
+    const onChange = useCallback(() => {
+
+            updateChar(getAllLength());
+
+        }),
+        onSelectionChange = useCallback((select) => {
+
+            updateChar(getSelectedLength(select));
+
+        });
     return (
         <div className="bg-gray-900 flex-auto">
             <AceEditor
@@ -56,5 +76,3 @@ export default function EditArea () {
     );
 
 }
-
-/* eslint-disable */
