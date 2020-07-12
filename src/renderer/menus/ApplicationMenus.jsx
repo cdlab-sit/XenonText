50446c60665220c5305c77baefc8d3470e692646
SuzuSho130 <cy15130@shibaura-it.ac.jp>
2020-07-12 18:52:58 +0900

    Mac用にappMenuを作成

--- src/renderer/menus/ApplicationMenus.jsx
+++ src/renderer/menus/ApplicationMenus.jsx
@@ -1,8 +1,33 @@
 import {editMenu, fileMenu, helpMenu, viewMenu} from "./index";
+import {app} from "electron";
+
+const isMac = process.platform === "darwin";
+
+let appMenu = [];
 
 export default function applicationMenuTemplate () {
 
+    if (isMac) {
+
+        appMenu = [
+            {
+                "label": app.name,
+                "submenu": [
+                    {
+                        "role": "quit"
+                    }
+                ]
+            }
+        ];
+
+    } else {
+
+        appMenu = [];
+
+    }
+
     return [
+        ...appMenu,
         fileMenu,
         editMenu,
         viewMenu,
