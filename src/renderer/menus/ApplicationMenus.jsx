e150a30642252c6cfe1ffc1b66d053eed9789dbc
SuzuSho130 <cy15130@shibaura-it.ac.jp>
2020-07-13 13:34:04 +0900

    ファイルのオープンとセーブの機能を追加

--- src/renderer/menus/ApplicationMenus.jsx
+++ src/renderer/menus/ApplicationMenus.jsx
@@ -1,58 +1,8 @@
 import {editMenu, fileMenu, helpMenu, viewMenu} from "./index";
 
-const
-    appName = "XenonText",
-    isMac = process.platform === "darwin";
-
-let appMenu = [];
-
-if (isMac) {
-
-    appMenu = [
-        {
-            "label": appName,
-            "submenu": [
-                {
-                    "role": "about"
-                },
-                {
-                    "type": "separator"
-                },
-                {
-                    "role": "services"
-                },
-                {
-                    "type": "separator"
-                },
-                {
-                    "role": "hide"
-                },
-                {
-                    "role": "hideothers"
-                },
-                {
-                    "role": "unhide"
-                },
-                {
-                    "type": "separator"
-                },
-                {
-                    "role": "quit"
-                }
-            ]
-        }
-    ];
-
-} else {
-
-    appMenu = [];
-
-}
-
 export default function applicationMenuTemplate () {
 
     return [
-        ...appMenu,
         fileMenu,
         editMenu,
         viewMenu,
