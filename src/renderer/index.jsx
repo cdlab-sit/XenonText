0801bdcd701e7158d20416f1445073e50da9634f
higurashi-takuto <contact@hgrs.me>
2020-07-24 19:25:17 +0900

    import 順と const 指定を変更

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -1,15 +1,15 @@
-import App from './App';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { applicationMenuTemplate } from './menus/index';
-import createStore from './reducks/store/store';
 import { remote } from 'electron';
+import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import createStore from './reducks/store/store';
+import { applicationMenuTemplate } from './menus/index';
 
 const applicationMenu = remote.Menu.buildFromTemplate(
-    applicationMenuTemplate(),
-  ),
-  store = createStore();
+  applicationMenuTemplate(),
+);
+const store = createStore();
 
 remote.Menu.setApplicationMenu(applicationMenu);
 
