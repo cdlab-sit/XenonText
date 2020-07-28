3546956169d9959ff9114031747ee3d70fbe3dde
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-07-28 19:39:36 +0900

    動的タブ追加機能 (editorId挿入版)

--- src/renderer/components/EditArea.jsx
+++ src/renderer/components/EditArea.jsx
@@ -12,60 +12,73 @@ import {
   setActiveEditorId,
 } from '../reducks/editor/actions';
 
-const EditArea = React.memo(function EditArea(props) {
-  let editorInstance = null;
-  const { editorId } = props;
-  const { initialText } = props;
-  console.log('start EditArea!!!!');
+let editorInstance = null;
+const EditArea = React.memo(
+  (props) => {
 
-  const dispatch = useDispatch();
-  const onChange = () => {
-    dispatch(setText(editorInstance));
-  };
-  const onLoad = (newEditorInstance) => {
-    editorInstance = newEditorInstance;
-    dispatch(setText(editorInstance));
-    // ファイルが新規作成された場合
-    if (editorId === '') {
+    console.log('start EditArea------: ');
+
+    // const initialText = props.fileText;
+    const initialText = 'initialText';
+
+    console.log('start =', props.editorId);
+
+    const dispatch = useDispatch();
+    const onChange = () => {
+      dispatch(setText(editorInstance));
+    };
+    const onLoad = (newEditorInstance) => {
+      editorInstance = newEditorInstance;
       dispatch(setMyEditorId(editorInstance.id));
       dispatch(setActiveEditorId(editorInstance.id));
+      dispatch(setText(editorInstance));
+    };
+    const onSelectionChange = () => {
+      dispatch(setSelectedText(editorInstance));
+    };
+    const onFocus = () => {
+      // console.log('onFocus\n\n', props.editorId);
+    };
+
+    return (
+      <div className="bg-gray-900 flex-auto">
+        <AceEditor
+          defaultValue={initialText}
+          editorProps={{ $blockScrolling: 'true' }}
+          fontSize="16px"
+          height="100%"
+          highlightActiveLine={false}
+          mode="c_cpp"
+          name="UNIQUE_ID_OF_DIV"
+          onChange={onChange}
+          onLoad={onLoad}
+          onSelectionChange={onSelectionChange}
+          onFocus={onFocus}
+          showPrintMargin={false}
+          tabSize={4}
+          theme="xenon"
+          width="100%"
+          wrapEnabed={false}
+        />
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    console.log('<', nextProps.editorId, '>');
+    console.log(prevProps, ' -> Props ->', nextProps);
+    if (prevProps.fileText === nextProps.fileText) {
+      console.log(true);
+      return true;
     }
-  };
-  const onSelectionChange = () => {
-    dispatch(setSelectedText(editorInstance));
-  };
-  const onFocus = () => {
-    // console.log('onFocus\n\n', props.editorId);
-  };
+    console.log(false);
+    return false;
+  },
+);
 
-  return (
-    <div className="bg-gray-900 flex-auto">
-      <AceEditor
-        defaultValue={initialText}
-        editorProps={{ $blockScrolling: 'true' }}
-        fontSize="16px"
-        height="100%"
-        highlightActiveLine={false}
-        mode="c_cpp"
-        name="UNIQUE_ID_OF_DIV"
-        onChange={onChange}
-        onLoad={onLoad}
-        onSelectionChange={onSelectionChange}
-        onFocus={onFocus}
-        showPrintMargin={false}
-        tabSize={4}
-        theme="xenon"
-        width="100%"
-        wrapEnabed={false}
-      />
-    </div>
-  );
-});
-
-EditArea.propTypes = {
-  editorId: PropTypes.string.isRequired,
-  initialText: PropTypes.string.isRequired,
-};
+// EditArea.propTypes = {
+//   editorId: PropTypes.string.isRequired,
+//   initialText: PropTypes.string.isRequired,
+// };
 
 export default EditArea;
 
--- src/renderer/components/Main.jsx
+++ src/renderer/components/Main.jsx
@@ -7,6 +7,7 @@ import { getActiveEditorId, getDocuments } from '../reducks/editor/selectors';
 
 export default function Main() {
   // EditAreaに文字入力時, なぜ2回呼び出されるのかは不明
+  // というか新規作成時, 2回呼び出されるからおかしくなってる
   console.log('start Main');
 
   const editorSelector = useSelector(
@@ -14,6 +15,7 @@ export default function Main() {
     // shallowEqualあってもかわんない
     shallowEqual,
   );
+
   const showId = getActiveEditorId(editorSelector);
   console.log('showId=', showId);
   const documents = getDocuments(editorSelector);
@@ -27,19 +29,24 @@ export default function Main() {
 
   return (
     <div className="flex flex-auto flex-col">
-      <Tabs />
-      {documents.map((document) => {
+      <Tabs documents={documents} />
+      {/* editorIdを更新すると不必要なeditorInstanceが作成される
+      多分, 別のものだと認識してReact.memoがつかえないんだろうな */}
+      {/* ちがいました, editorIdでkey指定してたからです!! */}
+      {documents.map((document, index) => {
         return (
           <div
             className={`flex flex-auto ${
               shouldShow(document.editorId) ? '' : 'hidden'
             }`}
-            key={document.editorId}
+            key={index}
           >
             <EditArea
-              editorId={document.editorId}
-              initialText={document.fileText}
-              key={document.editorId}
+              // editorId={document.editorId}
+              // initialText={document.fileText}
+              // key={document.fileText}
+              // fileText={"document.fileText"}
+              // key={document.fileText}
             />
           </div>
         );
