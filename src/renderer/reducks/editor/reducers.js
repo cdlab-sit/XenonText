b49b1ba1c131dd7718ad849d30d2f5daa6c1d8ac
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-07-28 19:56:09 +0900

    配列への書き込み方法変更

--- src/renderer/reducks/editor/reducers.js
+++ src/renderer/reducks/editor/reducers.js
@@ -5,47 +5,68 @@ import {
   SET_NEW_DOCUMENT,
   SET_MY_EDITOR_ID,
 } from './actions';
-import { getActiveDocument } from './selectors';
+import { getMyDocument } from './selectors';
 import initialState from '../store/initialState';
 
 const documentTemplate = {
+  // editorId: 'editor1',
   editorId: '',
   selectedText: null,
   editedText: '',
-  fileText: '',
+  fileText: 'documentTemplate',
   fileName: 'Untitled',
   filePath: null,
 };
-
 const EditorReducer = (state = initialState.editor, action) => {
   switch (action.type) {
     case SET_TEXT: {
-      console.log('start SET_TEXT');
-      const activeDocument = getActiveDocument(state);
+      console.log('set_text');
+      const { editorId } = action.payload;
+      console.log(editorId);
+      const document = getMyDocument(state, editorId);
+      console.log('document=', document, ' editorId=', editorId);
       const newDocument = {
-        ...activeDocument,
+        ...document,
         editedText: action.payload.text,
       };
       const newDocuments = state.documents.map((el) =>
-        el.editorId === activeDocument.editorId ? newDocument : el,
+        el.editorId === document.editorId ? newDocument : el,
       );
       return { ...state, documents: newDocuments };
     }
+
     case SET_SELECTED_TEXT: {
-      const activeDocument = getActiveDocument(state);
-      activeDocument.selectedText = action.payload.selectedText;
-      return { ...state };
+      const { editorId } = action.payload;
+      const document = getMyDocument(state, editorId);
+      const newDocument = {
+        ...document,
+        selectedText: action.payload.selectedText,
+      };
+      const newDocuments = state.documents.map((el) =>
+        el.editorId === document.editorId ? newDocument : el,
+      );
+      return { ...state, documents: newDocuments };
     }
+
     case SET_ACTIVE_EDITOR_ID:
+      console.log('activeEditorId->', action.payload.activeEditorId);
       return {
         ...state,
         ...action.payload,
       };
-    case SET_NEW_DOCUMENT:
-      return {
+    case SET_NEW_DOCUMENT: {
+      console.log('set_new_document: ');
+      console.log('state=', state);
+      console.log('documentTemplate', documentTemplate);
+      const newDocuments = {
         ...state,
         documents: [...state.documents, documentTemplate],
       };
+      console.log('newDoc', newDocuments);
+      return {
+        ...newDocuments,
+      };
+    }
     case SET_MY_EDITOR_ID: {
       const newDocument = {
         ...documentTemplate,
@@ -54,6 +75,7 @@ const EditorReducer = (state = initialState.editor, action) => {
       const newDocuments = state.documents.map((el) =>
         el.editorId === '' ? newDocument : el,
       );
+      console.log('state: ', {...state, documents: newDocuments});
       return { ...state, documents: newDocuments };
     }
     default:
@@ -61,3 +83,10 @@ const EditorReducer = (state = initialState.editor, action) => {
   }
 };
 export default EditorReducer;
+
+// よくない書き方
+// case SET_MY_EDITOR_ID: {
+//   const { editorId } = action.payload;
+//   const myDocument = getMyDocument(state, '');
+//   myDocument.editorId = editorId;
+//   return { ...state };
