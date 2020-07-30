02654bdc800b40acbf68209c3158ea9f69f43ef7
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-07-30 19:00:23 +0900

    プラスボタン追加

--- src/renderer/components/Tabs.jsx
+++ src/renderer/components/Tabs.jsx
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import Tab from './Tab';
 import { setNewDocument } from '../reducks/editor/actions';
 
+const newTabImagePathCommand =
+  'M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z';
+
 export default function Tabs(props) {
   const activeEditorId = useSelector((state) => state.editor.activeEditorId);
   const { documents } = props;
@@ -26,12 +29,22 @@ export default function Tabs(props) {
           />
         );
       })}
-      {/* デザインは適当 */}
+      {/* プラスボタン */}
       <div
-        className="flex-auto h-full bg-gray-500"
+        className="h-8 flex flex-auto items-center"
         onDoubleClick={onDoubleClick}
       >
-        タブルクリックで新規documentを作成
+        <svg
+          className="w-3 h-3 mx-2 text-gray-800"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path
+            clipRule="evenodd"
+            d={newTabImagePathCommand}
+            fillRule="evenodd"
+          />
+        </svg>
       </div>
     </div>
   );
