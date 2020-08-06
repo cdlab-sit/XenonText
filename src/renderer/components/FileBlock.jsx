a9cbd4c438dff6ae4196c75551089f2d8cfd5f0d
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-08-06 22:38:04 +0900

    FileSelectorの機能追加

--- src/renderer/components/FileBlock.jsx
+++ src/renderer/components/FileBlock.jsx
@@ -1,20 +1,48 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import FileStatus from './FileStatus';
+import { setActiveDocumentId } from '../reducks/editor/actions';
 
-export default function FileBlock() {
-  const item = 'untitled';
+const activeFileBlockColor = 'bg-gray-800';
+
+export default function FileBlock(props) {
+  const { title } = props;
+  const { documentId } = props;
+  const { isActive } = props;
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(setActiveDocumentId(documentId));
+  };
+
+  let fileBlockColor = '';
+  if (isActive) {
+    fileBlockColor = activeFileBlockColor;
+  }
 
   return (
-    <div className="flex h-6 w-full bg-gray-800 items-center">
-      {/* 現在未実装 */}
-      {/* <FileStatus /> */}
+    <div
+      className={`"h-6 w-full flex flex-row items-center " ${fileBlockColor}`}
+    >
+      <FileStatus documentId={documentId} />
       <h3
         className="
-                text-xs text-gray-300
-                leading-6 w-32 h-6 select-none
-                "
+          text-xs text-gray-300
+          leading-6 w-full h-6 select-none
+          "
+        onClick={onClick}
       >
-        {item}
+        {title}
       </h3>
     </div>
   );
 }
+
+FileBlock.propTypes = {
+  documentId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
