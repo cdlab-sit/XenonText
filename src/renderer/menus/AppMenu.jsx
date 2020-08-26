0c74b47c275286b1c5b6450e1efdf855c4c3b738
higurashi-takuto <contact@hgrs.me>
2020-08-26 12:11:43 +0900

    ファイルメニューの雛形作成

--- src/renderer/menus/AppMenu.jsx
+++ src/renderer/menus/AppMenu.jsx
@@ -1,16 +1,60 @@
+import fs from 'fs';
 import { remote } from 'electron';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setNewDocument } from '../reducks/editor/actions';
+import { setNewDocument, deleteDocument } from '../reducks/editor/actions';
 
 const { app, Menu } = remote;
 
 export default function AppMenu() {
   const dispatch = useDispatch();
+  const activeDocumentId = useSelector(
+    (state) => state.editor.activeDocumentId,
+  );
 
   // 新規ファイル
   const addNewFile = () => {
     dispatch(setNewDocument());
+  };
+
+  // ファイル読み込み
+  const readFile = (path) => {
+    fs.readFile(path, (error, data) => {
+      if (error !== null) {
+        throw error;
+      }
+      data.toString();
+    });
+  };
+
+  // ファイルを開く
+  const openFile = () => {
+    const fileIndex = 0;
+    const options = {
+      properties: ['openFile'],
+    };
+
+    remote.dialog.showOpenDialog(options).then((path) => {
+      if (path) {
+        // TODO: ファイル読み込みを完成させる（ Redux との連携 ）
+        readFile(path.filePaths[fileIndex]);
+      }
+    });
+  };
+
+  // ファイルの保存
+  const saveFile = () => {
+    // TODO: 実装する
+  };
+
+  // 名前を付けて保存
+  const saveFileAs = () => {
+    // TODO: TODO: 実装する
+  };
+
+  // ファイル（ タブ ）を閉じる
+  const closeFile = () => {
+    dispatch(deleteDocument(activeDocumentId));
   };
 
   const appName = app.name;
@@ -29,6 +73,7 @@ export default function AppMenu() {
         {
           label: 'サービス',
           role: 'services',
+          submenu: [],
         },
         {
           type: 'separator',
@@ -51,6 +96,46 @@ export default function AppMenu() {
         {
           label: `${appName} を終了`,
           role: 'quit',
+        },
+      ],
+    },
+    // ファイル
+    {
+      label: 'ファイル',
+      submenu: [
+        {
+          label: '新規ファイル',
+          accelerator: 'Command+N',
+          click: addNewFile,
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: '開く…',
+          accelerator: 'Command+O',
+          click: openFile,
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: '保存',
+          accelerator: 'Command+S',
+          click: saveFile,
+        },
+        {
+          label: '名前を付けて保存…',
+          accelerator: 'Shift+Command+S',
+          click: saveFileAs,
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'ファイルを閉じる',
+          accelerator: 'Command+W',
+          click: closeFile,
         },
       ],
     },
