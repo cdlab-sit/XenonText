ec6fd7b75bc179a589d8d78bfee130467395f85a
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-10-09 18:54:33 +0900

    visibility切り替え機能追加

--- src/renderer/components/Body.jsx
+++ src/renderer/components/Body.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ResizePanel from 'react-resize-panel';
+import { useSelector } from 'react-redux';
 import SideBar from './SideBar';
 import Main from './Main';
+import { getSideBarSettings } from '../reducks/settings/selectors';
 
 export default function Body() {
+  const sideBarSettings = getSideBarSettings(
+    useSelector((state) => state.settings),
+  );
+  let visibility = '';
+  if (sideBarSettings.visibility === false) {
+    visibility = 'none';
+  }
+
   return (
     <div className="flex flex-auto overflow-y-scroll">
       <ResizePanel
         direction="e"
         handleClass="hidden"
-        style={{ width: '200px' }}
+        style={{ width: '200px', display: visibility }}
       >
         <SideBar />
       </ResizePanel>
