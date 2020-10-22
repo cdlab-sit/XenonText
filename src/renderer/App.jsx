e64752466cbbe2887f8ecabc72f7f90ceca22624
higurashi-takuto <contact@hgrs.me>
2020-10-22 15:25:39 +0900

    クラスコンポーネントから関数コンポーネントに変更

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,48 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { remote } from 'electron';
 import Footer from './components/Footer';
 import Body from './components/Body';
 import TitleBar from './components/TitleBar';
 import AppMenu from './menus/AppMenu';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isFullScreen: false,
-    };
+export default function App() {
+  // スクリーン状態を保持
+  const [isFullScreen, setFullScreen] = useState(false);
 
-    const currentWindow = remote.getCurrentWindow();
+  // スクリーン状態の書き換え
+  const currentWindow = remote.getCurrentWindow();
+  currentWindow.on('enter-full-screen', () => {
+    setFullScreen(true);
+  });
+  currentWindow.on('leave-full-screen', () => {
+    setFullScreen(false);
+  });
 
-    currentWindow.on('enter-full-screen', () => {
-      this.setState({ isFullScreen: true });
-    });
-
-    currentWindow.on('leave-full-screen', () => {
-      this.setState({ isFullScreen: false });
-    });
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const { isFullScreen } = this.state;
-
-    if (isFullScreen !== nextState.isFullScreen) {
-      return true;
-    }
-
-    return false;
-  }
-
-  render() {
-    const { isFullScreen } = this.state;
-
-    return (
-      <div className="flex flex-col h-screen">
-        {!isFullScreen && <TitleBar />}
-        <Body />
-        <Footer />
-        <AppMenu />
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col h-screen">
+      {!isFullScreen && <TitleBar />}
+      <Body />
+      <Footer />
+      <AppMenu />
+    </div>
+  );
 }
